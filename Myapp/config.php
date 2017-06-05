6aec332ca657c8b7d169a4321dff498cad89d24e
jeff281 <jeff2891@gmail.com>
2017-06-05 18:06:15 -0400

    app

---
+++ Myapp/config.php
@@ -0,0 +1,7 @@
+<?php
+// This is an example of config.php
+$dbhost = 'us-cdbr-azure-southcentral-f.cloudapp.net';
+$dbuser = 'bdefd295e2be6f';
+$dbpass = '224735c1';
+$dbname = 'passwordsaver';
+?>
---
+++ Myapp/javascript.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+$(document).one('pageinit', function() {
+	// Add Handler
+	"use strict";
+	$('#submitAdd').on('tap', addCat);
+	
+	/*
+	* Add a catagory
+	*/
+	function addCat (){
+		// get form values
+		var category = $ ('#addCategoryName') .val();
+		alert(1);
+		// Create 'cat' object
+	
+		}
+	}
+	
+	
+);
